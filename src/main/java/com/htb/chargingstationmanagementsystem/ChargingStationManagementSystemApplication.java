49fb6e0c712df484d7e62b1abb0d95a6a87c79c9
reza.mazloom <reza.mazloom@external.iherb.com>
2022-03-09 01:49:21 +0300

    initial commit

---
+++ src/main/java/com/htb/chargingstationmanagementsystem/ChargingStationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.htb.chargingstationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingStationManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargingStationManagementSystemApplication.class, args);
+    }
+
+}
