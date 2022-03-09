d6acb41dfdaa06623ebc60e932208c16f4ae96af
reza.mazloom <reza.mazloom@external.iherb.com>
2022-03-09 04:07:55 +0300

    feat: add instructions on how to run the application

--- src/main/java/com/htb/chargingstationmanagementsystem/service/RateCalculator.java
+++ src/main/java/com/htb/chargingstationmanagementsystem/service/RateCalculator.java
@@ -13,19 +13,23 @@ public class RateCalculator implements Calculator {
     @Override
     public ChargingRateDto.Response rateChargingProcess(ChargingRateDto.Request request) {
         Component component = new Component();
+
         component.setEnergy(request.getRate().getEnergy()
                 .multiply(new BigDecimal(request.getCdr().getMeterStop() - request.getCdr().getMeterStart()))
                 .setScale(3, RoundingMode.HALF_UP)
                 .divide(new BigDecimal(1000), RoundingMode.HALF_UP));
+
         component.setTime(request.getRate().getTime()
                 .multiply(new BigDecimal(Duration.between(request.getCdr().getTimestampStart(),
                         request.getCdr().getTimestampStop()).toSeconds()))
                 .divide(new BigDecimal(3600), 4, RoundingMode.DOWN)
                 .setScale(3, RoundingMode.UP));
+
         component.setTransaction(request.getRate().getTransaction());
+
         ChargingRateDto.Response response = new ChargingRateDto.Response();
-        response.setOverall(component.getEnergy().add(component.getTime()).add(component.getTransaction()).setScale(2
-                , RoundingMode.HALF_DOWN));
+        response.setOverall(component.getEnergy().add(component.getTime()).add(component.getTransaction())
+                .setScale(2, RoundingMode.HALF_DOWN));
         response.setComponents(component);
         return response;
     }
